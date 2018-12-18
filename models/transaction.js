fc2e4537fccee4c31cdeb12249f7768862ce229a
piyush27ranjan <piyush.27ranjan@gmail.com>
2018-12-19 00:22:07 +0530

    Added transaction schema

---
+++ models/transaction.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+var schema = mongoose.Schema;
+
+var UserSchema = new schema({
+    book_name: {type: String, required: [true, "book name is required"]},
+    payable_user: {type: String, required: [true, "payer required"]},
+    expense: {type: Number, required: [true, "Expense value required"]},
+    borrowers: {type: Array},
+    purpose: {type: String}
+});
+
+module.exports = mongoose.model('transaction', UserSchema);
