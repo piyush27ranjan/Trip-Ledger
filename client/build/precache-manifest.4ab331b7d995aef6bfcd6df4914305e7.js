90d050a7a80244e00af47358407f4824550d4760
piyush27ranjan <piyush.27ranjan@gmail.com>
2021-04-14 11:10:37 +0530

    build folder

---
+++ client/build/precache-manifest.4ab331b7d995aef6bfcd6df4914305e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2482e69aff420519dcc4",
+    "url": "/static/js/main.2482e69a.chunk.js"
+  },
+  {
+    "revision": "6a105fabe05d39b8afd9",
+    "url": "/static/js/1.6a105fab.chunk.js"
+  },
+  {
+    "revision": "2482e69aff420519dcc4",
+    "url": "/static/css/main.900d126e.chunk.css"
+  },
+  {
+    "revision": "824b5e823e50a164acdbe14f1384d9f2",
+    "url": "/index.html"
+  }
+];
